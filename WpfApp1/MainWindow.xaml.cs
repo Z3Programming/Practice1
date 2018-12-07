38fb542cc394afc7804bcc15f1e2a52ca20c9f42
Z3Programming <z3programming@gmail.com>
2018-12-06 19:30:24 -0500

    Added baTag dictionary

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,14 +20,31 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //declare carrier name array
+        public string[] carrierNames { get; set; }
+        
+        //set BA Tags
+        public Dictionary<string,string> baTags =  new Dictionary<string, string>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //build carrier names array
+            carrierNames = new string[] { "","Avatar Book Roll","Avatar Rewrite Book Roll","Geovera Book Roll","Safeco Book Roll","Tower Hill Book Roll" };
+
+            DataContext = this;
+
+            //build carrier name dictionary
+            baTags.Add("Avatar Book Roll","abc");
+            baTags.Add("Avatar Rewrite Book Roll", "def");
+            baTags.Add("Geovera Book Roll", "ghi");
+            baTags.Add("Safeco Book Roll", "jkl");
+            baTags.Add("Tower Hill Book Roll", "mno");
+
+
         }
 
-        private void SubmitButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hello World");
-        }
+
     }
 }
